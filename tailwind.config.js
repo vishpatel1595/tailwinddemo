868721256f05bcd7f9aee87afcfdc392b414ba8f
Vishal <vishpatel1595@gmail.com>
2023-07-28 17:56:53 +0530

    css changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html"],
+  theme: {
+    extend: {},
+    color: {
+      bgblue:"#2d5596",
+    }
+  },
+  plugins: [],
+}
+
